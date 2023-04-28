fa18b41c1ccc5ffff5d9809039901bc15532092e
KtranH <87740741+KtranH@users.noreply.github.com>
2023-04-28 15:12:45 +0700

    Upload first time

---
+++ script.js
@@ -0,0 +1,473 @@
+ const bannerImages = document.querySelectorAll('.banner img');
+
+  // Set index to 0 to start with the first image
+  let index = 0;
+
+  // Set timer to change image every 5 seconds
+  setInterval(function() {
+    // Remove the "active" class from the current image
+    bannerImages[index].classList.remove('active');
+
+    // Increment the index or reset to 0 if at the end of the images
+    index = (index + 1) % bannerImages.length;
+
+    // Add the "active" class to the next image
+    bannerImages[index].classList.add('active');
+  }, 5000); const bannerImages = document.querySelectorAll('.banner img');
+
+  // Set index to 0 to start with the first image
+  let index = 0;
+
+  // Set timer to change image every 5 seconds
+  setInterval(function() {
+    // Remove the "active" class from the current image
+    bannerImages[index].classList.remove('active');
+
+    // Increment the index or reset to 0 if at the end of the images
+    index = (index + 1) % bannerImages.length;
+
+    // Add the "active" class to the next image
+    bannerImages[index].classList.add('active');
+  }, 5000); const bannerImages = document.querySelectorAll('.banner img');
+
+  // Set index to 0 to start with the first image
+  let index = 0;
+
+  // Set timer to change image every 5 seconds
+  setInterval(function() {
+    // Remove the "active" class from the current image
+    bannerImages[index].classList.remove('active');
+
+    // Increment the index or reset to 0 if at the end of the images
+    index = (index + 1) % bannerImages.length;
+
+    // Add the "active" class to the next image
+    bannerImages[index].classList.add('active');
+  }, 5000); const bannerImages = document.querySelectorAll('.banner img');
+
+  // Set index to 0 to start with the first image
+  let index = 0;
+
+  // Set timer to change image every 5 seconds
+  setInterval(function() {
+    // Remove the "active" class from the current image
+    bannerImages[index].classList.remove('active');
+
+    // Increment the index or reset to 0 if at the end of the images
+    index = (index + 1) % bannerImages.length;
+
+    // Add the "active" class to the next image
+    bannerImages[index].classList.add('active');
+  }, 5000); const bannerImages = document.querySelectorAll('.banner img');
+
+  // Set index to 0 to start with the first image
+  let index = 0;
+
+  // Set timer to change image every 5 seconds
+  setInterval(function() {
+    // Remove the "active" class from the current image
+    bannerImages[index].classList.remove('active');
+
+    // Increment the index or reset to 0 if at the end of the images
+    index = (index + 1) % bannerImages.length;
+
+    // Add the "active" class to the next image
+    bannerImages[index].classList.add('active');
+  }, 5000); const bannerImages = document.querySelectorAll('.banner img');
+
+  // Set index to 0 to start with the first image
+  let index = 0;
+
+  // Set timer to change image every 5 seconds
+  setInterval(function() {
+    // Remove the "active" class from the current image
+    bannerImages[index].classList.remove('active');
+
+    // Increment the index or reset to 0 if at the end of the images
+    index = (index + 1) % bannerImages.length;
+
+    // Add the "active" class to the next image
+    bannerImages[index].classList.add('active');
+  }, 5000); const bannerImages = document.querySelectorAll('.banner img');
+
+  // Set index to 0 to start with the first image
+  let index = 0;
+
+  // Set timer to change image every 5 seconds
+  setInterval(function() {
+    // Remove the "active" class from the current image
+    bannerImages[index].classList.remove('active');
+
+    // Increment the index or reset to 0 if at the end of the images
+    index = (index + 1) % bannerImages.length;
+
+    // Add the "active" class to the next image
+    bannerImages[index].classList.add('active');
+  }, 5000); const bannerImages = document.querySelectorAll('.banner img');
+
+  // Set index to 0 to start with the first image
+  let index = 0;
+
+  // Set timer to change image every 5 seconds
+  setInterval(function() {
+    // Remove the "active" class from the current image
+    bannerImages[index].classList.remove('active');
+
+    // Increment the index or reset to 0 if at the end of the images
+    index = (index + 1) % bannerImages.length;
+
+    // Add the "active" class to the next image
+    bannerImages[index].classList.add('active');
+  }, 5000); const bannerImages = document.querySelectorAll('.banner img');
+
+  // Set index to 0 to start with the first image
+  let index = 0;
+
+  // Set timer to change image every 5 seconds
+  setInterval(function() {
+    // Remove the "active" class from the current image
+    bannerImages[index].classList.remove('active');
+
+    // Increment the index or reset to 0 if at the end of the images
+    index = (index + 1) % bannerImages.length;
+
+    // Add the "active" class to the next image
+    bannerImages[index].classList.add('active');
+  }, 5000); const bannerImages = document.querySelectorAll('.banner img');
+
+  // Set index to 0 to start with the first image
+  let index = 0;
+
+  // Set timer to change image every 5 seconds
+  setInterval(function() {
+    // Remove the "active" class from the current image
+    bannerImages[index].classList.remove('active');
+
+    // Increment the index or reset to 0 if at the end of the images
+    index = (index + 1) % bannerImages.length;
+
+    // Add the "active" class to the next image
+    bannerImages[index].classList.add('active');
+  }, 5000); const bannerImages = document.querySelectorAll('.banner img');
+
+  // Set index to 0 to start with the first image
+  let index = 0;
+
+  // Set timer to change image every 5 seconds
+  setInterval(function() {
+    // Remove the "active" class from the current image
+    bannerImages[index].classList.remove('active');
+
+    // Increment the index or reset to 0 if at the end of the images
+    index = (index + 1) % bannerImages.length;
+
+    // Add the "active" class to the next image
+    bannerImages[index].classList.add('active');
+  }, 5000); const bannerImages = document.querySelectorAll('.banner img');
+
+  // Set index to 0 to start with the first image
+  let index = 0;
+
+  // Set timer to change image every 5 seconds
+  setInterval(function() {
+    // Remove the "active" class from the current image
+    bannerImages[index].classList.remove('active');
+
+    // Increment the index or reset to 0 if at the end of the images
+    index = (index + 1) % bannerImages.length;
+
+    // Add the "active" class to the next image
+    bannerImages[index].classList.add('active');
+  }, 5000);const bannerImages = document.querySelectorAll('.banner img');
+
+// Set index to 0 to start with the first image
+let index = 0;
+
+// Set timer to change image every 5 seconds
+setInterval(function() {
+  // Remove the "active" class from the current image
+  bannerImages[index].classList.remove('active');
+
+  // Increment the index or reset to 0 if at the end of the images
+  index = (index + 1) % bannerImages.length;
+
+  // Add the "active" class to the next image
+  bannerImages[index].classList.add('active');
+}, 5000);
+const bannerImages = document.querySelectorAll('.banner img');
+
+// Set index to 0 to start with the first image
+let index = 0;
+
+// Set timer to change image every 5 seconds
+setInterval(function() {
+  // Remove the "active" class from the current image
+  bannerImages[index].classList.remove('active');
+
+  // Increment the index or reset to 0 if at the end of the images
+  index = (index + 1) % bannerImages.length;
+
+  // Add the "active" class to the next image
+  bannerImages[index].classList.add('active');
+}, 5000);
+const bannerImages = document.querySelectorAll('.banner img');
+
+// Set index to 0 to start with the first image
+let index = 0;
+
+// Set timer to change image every 5 seconds
+setInterval(function() {
+  // Remove the "active" class from the current image
+  bannerImages[index].classList.remove('active');
+
+  // Increment the index or reset to 0 if at the end of the images
+  index = (index + 1) % bannerImages.length;
+
+  // Add the "active" class to the next image
+  bannerImages[index].classList.add('active');
+}, 5000);
+const bannerImages = document.querySelectorAll('.banner img');
+
+// Set index to 0 to start with the first image
+let index = 0;
+
+// Set timer to change image every 5 seconds
+setInterval(function() {
+  // Remove the "active" class from the current image
+  bannerImages[index].classList.remove('active');
+
+  // Increment the index or reset to 0 if at the end of the images
+  index = (index + 1) % bannerImages.length;
+
+  // Add the "active" class to the next image
+  bannerImages[index].classList.add('active');
+}, 5000);
+const bannerImages = document.querySelectorAll('.banner img');
+
+// Set index to 0 to start with the first image
+let index = 0;
+
+// Set timer to change image every 5 seconds
+setInterval(function() {
+  // Remove the "active" class from the current image
+  bannerImages[index].classList.remove('active');
+
+  // Increment the index or reset to 0 if at the end of the images
+  index = (index + 1) % bannerImages.length;
+
+  // Add the "active" class to the next image
+  bannerImages[index].classList.add('active');
+}, 5000);
+const bannerImages = document.querySelectorAll('.banner img');
+
+// Set index to 0 to start with the first image
+let index = 0;
+
+// Set timer to change image every 5 seconds
+setInterval(function() {
+  // Remove the "active" class from the current image
+  bannerImages[index].classList.remove('active');
+
+  // Increment the index or reset to 0 if at the end of the images
+  index = (index + 1) % bannerImages.length;
+
+  // Add the "active" class to the next image
+  bannerImages[index].classList.add('active');
+}, 5000);
+const bannerImages = document.querySelectorAll('.banner img');
+
+// Set index to 0 to start with the first image
+let index = 0;
+
+// Set timer to change image every 5 seconds
+setInterval(function() {
+  // Remove the "active" class from the current image
+  bannerImages[index].classList.remove('active');
+
+  // Increment the index or reset to 0 if at the end of the images
+  index = (index + 1) % bannerImages.length;
+
+  // Add the "active" class to the next image
+  bannerImages[index].classList.add('active');
+}, 5000);
+const bannerImages = document.querySelectorAll('.banner img');
+
+// Set index to 0 to start with the first image
+let index = 0;
+
+// Set timer to change image every 5 seconds
+setInterval(function() {
+  // Remove the "active" class from the current image
+  bannerImages[index].classList.remove('active');
+
+  // Increment the index or reset to 0 if at the end of the images
+  index = (index + 1) % bannerImages.length;
+
+  // Add the "active" class to the next image
+  bannerImages[index].classList.add('active');
+}, 5000);const bannerImages = document.querySelectorAll('.banner img');
+
+// Set index to 0 to start with the first image
+let index = 0;
+
+// Set timer to change image every 5 seconds
+setInterval(function() {
+  // Remove the "active" class from the current image
+  bannerImages[index].classList.remove('active');
+
+  // Increment the index or reset to 0 if at the end of the images
+  index = (index + 1) % bannerImages.length;
+
+  // Add the "active" class to the next image
+  bannerImages[index].classList.add('active');
+}, 5000);const bannerImages = document.querySelectorAll('.banner img');
+
+// Set index to 0 to start with the first image
+let index = 0;
+
+// Set timer to change image every 5 seconds
+setInterval(function() {
+  // Remove the "active" class from the current image
+  bannerImages[index].classList.remove('active');
+
+  // Increment the index or reset to 0 if at the end of the images
+  index = (index + 1) % bannerImages.length;
+
+  // Add the "active" class to the next image
+  bannerImages[index].classList.add('active');
+}, 5000);const bannerImages = document.querySelectorAll('.banner img');
+
+// Set index to 0 to start with the first image
+let index = 0;
+
+// Set timer to change image every 5 seconds
+setInterval(function() {
+  // Remove the "active" class from the current image
+  bannerImages[index].classList.remove('active');
+
+  // Increment the index or reset to 0 if at the end of the images
+  index = (index + 1) % bannerImages.length;
+
+  // Add the "active" class to the next image
+  bannerImages[index].classList.add('active');
+}, 5000);const bannerImages = document.querySelectorAll('.banner img');
+
+// Set index to 0 to start with the first image
+let index = 0;
+
+// Set timer to change image every 5 seconds
+setInterval(function() {
+  // Remove the "active" class from the current image
+  bannerImages[index].classList.remove('active');
+
+  // Increment the index or reset to 0 if at the end of the images
+  index = (index + 1) % bannerImages.length;
+
+  // Add the "active" class to the next image
+  bannerImages[index].classList.add('active');
+}, 5000);const bannerImages = document.querySelectorAll('.banner img');
+
+// Set index to 0 to start with the first image
+let index = 0;
+
+// Set timer to change image every 5 seconds
+setInterval(function() {
+  // Remove the "active" class from the current image
+  bannerImages[index].classList.remove('active');
+
+  // Increment the index or reset to 0 if at the end of the images
+  index = (index + 1) % bannerImages.length;
+
+  // Add the "active" class to the next image
+  bannerImages[index].classList.add('active');
+}, 5000);const bannerImages = document.querySelectorAll('.banner img');
+
+// Set index to 0 to start with the first image
+let index = 0;
+
+// Set timer to change image every 5 seconds
+setInterval(function() {
+  // Remove the "active" class from the current image
+  bannerImages[index].classList.remove('active');
+
+  // Increment the index or reset to 0 if at the end of the images
+  index = (index + 1) % bannerImages.length;
+
+  // Add the "active" class to the next image
+  bannerImages[index].classList.add('active');
+}, 5000);const bannerImages = document.querySelectorAll('.banner img');
+
+// Set index to 0 to start with the first image
+let index = 0;
+
+// Set timer to change image every 5 seconds
+setInterval(function() {
+  // Remove the "active" class from the current image
+  bannerImages[index].classList.remove('active');
+
+  // Increment the index or reset to 0 if at the end of the images
+  index = (index + 1) % bannerImages.length;
+
+  // Add the "active" class to the next image
+  bannerImages[index].classList.add('active');
+}, 5000);const bannerImages = document.querySelectorAll('.banner img');
+
+// Set index to 0 to start with the first image
+let index = 0;
+
+// Set timer to change image every 5 seconds
+setInterval(function() {
+  // Remove the "active" class from the current image
+  bannerImages[index].classList.remove('active');
+
+  // Increment the index or reset to 0 if at the end of the images
+  index = (index + 1) % bannerImages.length;
+
+  // Add the "active" class to the next image
+  bannerImages[index].classList.add('active');
+}, 5000);const bannerImages = document.querySelectorAll('.banner img');
+
+// Set index to 0 to start with the first image
+let index = 0;
+
+// Set timer to change image every 5 seconds
+setInterval(function() {
+  // Remove the "active" class from the current image
+  bannerImages[index].classList.remove('active');
+
+  // Increment the index or reset to 0 if at the end of the images
+  index = (index + 1) % bannerImages.length;
+
+  // Add the "active" class to the next image
+  bannerImages[index].classList.add('active');
+}, 5000);const bannerImages = document.querySelectorAll('.banner img');
+
+// Set index to 0 to start with the first image
+let index = 0;
+
+// Set timer to change image every 5 seconds
+setInterval(function() {
+  // Remove the "active" class from the current image
+  bannerImages[index].classList.remove('active');
+
+  // Increment the index or reset to 0 if at the end of the images
+  index = (index + 1) % bannerImages.length;
+
+  // Add the "active" class to the next image
+  bannerImages[index].classList.add('active');
+}, 5000);const bannerImages = document.querySelectorAll('.banner img');
+
+// Set index to 0 to start with the first image
+let index = 0;
+
+// Set timer to change image every 5 seconds
+setInterval(function() {
+  // Remove the "active" class from the current image
+  bannerImages[index].classList.remove('active');
+
+  // Increment the index or reset to 0 if at the end of the images
+  index = (index + 1) % bannerImages.length;
+
+  // Add the "active" class to the next image
+  bannerImages[index].classList.add('active');
+}, 5000);
